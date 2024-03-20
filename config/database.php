e2c2a54173973300181de633fe431629a813f773
Fietra Prabaskara <kara.astan@gmail.com>
2024-03-20 23:46:30 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://default:NqSjImn9U0yf@ep-super-dawn-a7ot2nzp-pooler.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require',
+            'host' => 'ep-super-dawn-a7ot2nzp-pooler.ap-southeast-2.aws.neon.tech',
+            'port' => '5432',
+            'database' => 'verceldb',
+            'username' => 'default',
+            'password' => 'NqSjImn9U0yf',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
